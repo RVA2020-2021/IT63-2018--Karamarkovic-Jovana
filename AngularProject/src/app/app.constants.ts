9b6e50ce8f98a93afab3f3b0d5efd616ec7c817b
Jovana <jocakaramarkovic@gmail.com>
2021-06-25 17:02:41 +0200

    Deploy

--- AngularProject/src/app/app.constants.ts
+++ AngularProject/src/app/app.constants.ts
@@ -1,5 +1,11 @@
-export const RACUN_URL = 'http://localhost:8083/racun';
-export const PROIZVODJAC_URL = 'http://localhost:8083/proizvodjac';
-export const PROIZVOD_URL = 'http://localhost:8083/proizvod';
-export const STAVKA_RACUNA_URL = 'http://localhost:8083/stavkaRacuna';
-export const STAVKE_ZA_RACUNE_URL = 'http://localhost:8083/stavkaZaRacuneId';
+export const RACUN_URL = 'https://it63g2018-rva.herokuapp.com/racun';
+export const PROIZVODJAC_URL = 'https://it63g2018-rva.herokuapp.com/proizvodjac';
+export const PROIZVOD_URL = 'https://it63g2018-rva.herokuapp.com/proizvod';
+export const STAVKA_RACUNA_URL = 'https://it63g2018-rva.herokuapp.com/stavkaRacuna';
+export const STAVKE_ZA_RACUNE_URL = 'https://it63g2018-rva.herokuapp.com/stavkaZaRacuneId';
+
+// export const RACUN_URL = 'http://localhost:8083/racun';
+// export const PROIZVODJAC_URL = 'http://localhost:8083/proizvodjac';
+// export const PROIZVOD_URL = 'http://localhost:8083/proizvod';
+// export const STAVKA_RACUNA_URL = 'http://localhost:8083/stavkaRacuna';
+// export const STAVKE_ZA_RACUNE_URL = 'http://localhost:8083/stavkaZaRacuneId';
---
+++ README.md
@@ -0,0 +1,5 @@
+# RVA Projekat
+
+API: https://it63g2018-rva.herokuapp.com/
+
+Klijent: https://sleepy-babbage-c125c3.netlify.app/
